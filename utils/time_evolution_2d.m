0c1f8447f899a3d9807a8c654dfd37c1d9565d34
rask24 <rask0x18@gmail.com>
2023-01-11 14:40:48 +0900

    implement window

--- utils/time_evolution_2d.m
+++ utils/time_evolution_2d.m
@@ -1,16 +1,18 @@
 function [ret1, ret2] = time_evolution_2d(input1, input2, mod)
+    theta = 0.005;
+    mat = [cos(theta) -sin(theta); sin(theta) cos(theta)];
     if mod == 1
-        theta = 0.005;
-        mat = [cos(theta) -sin(theta); sin(theta) cos(theta)];
-    elseif mod == 2
-        theta = 0.0001;
-        mat = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+        r = 1.001;
+        add = 0;
     else
-        mat = eye(2);
+        r = 1.01;
+        add = 0.0001;
     end
     tmp = mat * [input1 ; input2];
     ret1 = [1 0] * tmp;
     ret2 = [0 1] * tmp;
+    ret1 = r * ret1 + add;
+    ret2 = r * ret2;
     % ret1  = ret1 + sin(ret2);
     % ret1  = ret1 + cos(ret1);
     % ret1 = [1.001 0] * tmp;
